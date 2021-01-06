7b4fa1aad1ccc29ff02523221071134b03ed3039
Ivan Buetler <ivan.buetler@compass-security.com>
2021-01-06 12:21:44 +0100

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 docker build --no-cache -t hackinglab/alpine-apache2-reverse-proxy:3.2.0 -t hackinglab/alpine-apache2-reverse-proxy:3.2 -t hackinglab/alpine-apache2-reverse-proxy:latest -f Dockerfile .
 
 docker push hackinglab/alpine-apache2-reverse-proxy
+docker push hackinglab/alpine-apache2-reverse-proxy:3.2
+docker push hackinglab/alpine-apache2-reverse-proxy:3.2.0
 
